b3f1bf99418f9a4db5239fbf0aa7935bbac2f217
HackMemory <dragon.warri0r@yandex.com>
2023-06-25 01:15:16 +0300

    add whisper recognizer

---
+++ app/speech/recognizer.py
@@ -0,0 +1,30 @@
+import whisper
+from config import config
+
+def format_item(item):
+    return {
+        "time": item["start"],
+        "text": item["text"]
+    }
+
+class Recognizer:
+    model = None
+
+    def __init__(self):
+        self.model = whisper.load_model(config.whisper_model_name)
+
+    def transcribe(self, path):
+        result = self.model.transcribe(path)
+
+        segments = []
+        for item in result["segments"]:
+            segments.append(format_item(item))
+
+        data = {
+            "text": result['text'],
+            "segments": segments
+        }
+
+        return data
+
+recognizer = Recognizer()
